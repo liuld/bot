a14cd830164f453df6d3e4c89d775c4ca76a89d6
liuld <liuldgm@gmain.com>
2020-04-12 22:31:23 +0800

    bot

--- mongo.py
+++ mongo.py
@@ -7,8 +7,11 @@ import pymongo
 
 class Mongo(object):
 
-    def __init__(self, host='localhost', port=27017):
+    def __init__(self, host='localhost', port=27017, username=None, password=None):
         self.client = pymongo.MongoClient(host=host, port=port)
+        if username:
+            admin = self.client['admin']
+            admin.authenticate(username, password)
 
     def get_db_cursor(self, db):
         return self.client[db]
